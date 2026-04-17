af1469df0cb3f0d321f603b2dc97fd8fc9b7af5a
Shivam-GitLab <shivam_223033@saitm.org>
2026-04-17 15:00:33 +0530

    chore: initialize Wellnow - Spring Boot Project

---
+++ src/main/java/com/sm/wellnow/WellNowApplication.java
@@ -0,0 +1,13 @@
+package com.sm.wellnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sm/wellnow/WellNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.wellnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
