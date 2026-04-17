10d2bf81411917c9d277eda7039d3e2a481278e2
Shivam-GitLab <shivam_223033@saitm.org>
2026-04-17 15:20:04 +0530

    feat(repositoty) add repositroy

---
+++ src/main/java/com/sm/wellnow/repository/ActivityRepository.java
@@ -0,0 +1,13 @@
+package com.sm.wellnow.repository;
+
+
+import com.sm.wellnow.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, String> {
+    List<Activity> findByUserId(String userId);
+}
---
+++ src/main/java/com/sm/wellnow/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sm.wellnow.repository;
+
+import com.sm.wellnow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
