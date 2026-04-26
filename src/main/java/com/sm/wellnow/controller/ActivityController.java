f680c335ef49008bbdfe541288181df11cfb448c
Shivam-GitLab <shivam_223033@saitm.org>
2026-04-26 23:50:49 +0530

    feat - add activity api

---
+++ src/main/java/com/sm/wellnow/controller/ActivityController.java
@@ -0,0 +1,11 @@
+package com.sm.wellnow.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/activities")
+@RequiredArgsConstructor
+public class ActivityController {
+}
