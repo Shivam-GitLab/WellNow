172882a24663317e72bbf211c2508b200c6f34f0
Shivam-GitLab <shivam_223033@saitm.org>
2026-06-04 10:53:25 +0530

    updtae - security done start jwt

--- src/main/java/com/sm/wellnow/WellNowApplication.java
+++ src/main/java/com/sm/wellnow/WellNowApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WellNowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WellNowApplication.class, args);
     }
-
 }
--- src/main/java/com/sm/wellnow/config/SecurityConfig.java
+++ src/main/java/com/sm/wellnow/config/SecurityConfig.java
@@ -1,50 +1,6 @@
-/*
 package com.sm.wellnow.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated()
-        );
-        http.httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
-                .roles("USER") // ROLE_USER
-                .build();
-        UserDetails user2 = User.withUsername("admin1")
-                .password("{noop}admin123")
-                .roles("ADMIN") // ROLE_ADMIN - Internally
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
-
-}
-*/
-
-package com.sm.wellnow.config;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -55,13 +11,20 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -73,13 +36,18 @@ public class SecurityConfig {
                 // Authorization Rules
                 .authorizeHttpRequests(auth -> auth
 
-//                        .requestMatchers("/admin/**")
-//                        .hasAuthority("ROLE_ADMIN")
-//
-//                        .requestMatchers("/user/**")
-//                        .hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                  /*    .requestMatchers("/admin/**")
+                        .hasAuthority("ROLE_ADMIN")
 
-                        .anyRequest().authenticated()
+                        .requestMatchers("/user/**")
+                        .hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+
+                         .requestMatchers("/admin/**")
+                         .hasRole("ADMIN")
+                         .requestMatchers("/user/**")
+                         .hasAnyRole("USER", "ADMIN")
+*/
+                         .anyRequest().authenticated()
                 )
 
                 // Basic Authentication
@@ -100,16 +68,34 @@ public class SecurityConfig {
     @Bean
     UserDetailsService userDetailsService() {
 
-        UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+        UserDetails user1 = User.withUsername("user123")
+//                .password("{noop}password1")
+                .password(passwordEncoder().encode("user123"))
                 .roles("USER")
                 .build();
 
-        UserDetails user2 = User.withUsername("admin1")
-                .password("{noop}admin123")
+        UserDetails user2 = User.withUsername("admin123")
+//                .password("{noop}admin123")
+                .password(passwordEncoder().encode("admin123"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1, user2);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Only create users if they don't already exist
+
+        if (!userDetailsManager.userExists("user1")) {
+            userDetailsManager.createUser(user1);
+        }
+        if (!userDetailsManager.userExists("admin1")) {
+            userDetailsManager.createUser(user2);
+        }
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
